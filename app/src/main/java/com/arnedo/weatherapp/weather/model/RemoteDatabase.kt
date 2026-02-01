fd9d6c9700db469f4dff64529c62f84e782007f0
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-01 21:25:44 +0100

    add RemoteDatabase.kt for remote weather data access

---
+++ app/src/main/java/com/arnedo/weatherapp/weather/model/RemoteDatabase.kt
@@ -0,0 +1,40 @@
+package com.arnedo.weatherapp.weather.model
+
+import com.arnedo.weatherapp.common.entities.WeatherCity
+import com.arnedo.weatherapp.common.utils.Constants
+import com.arnedo.weatherapp.common.utils.FormatUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RemoteDatabase(
+    private val service: WeatherService,
+    private val utils : FormatUtils) {
+    private fun setupRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun weatherService() : WeatherService {
+        return setupRetrofit().create(WeatherService::class.java)
+    }
+
+    suspend fun searchWeatherByName(
+        name: String,
+        onResult : (WeatherCity?) -> Unit)
+    = withContext(Dispatchers.IO){
+        try {
+            val result = weatherService().searchWeatherByName(
+                key = Constants.BASE_URL,
+                name = name,
+                lang = Constants.LANG_ES
+            )
+            onResult(utils.responseToWeatherCity(result))
+        }catch(e: Exception){
+            onResult(null)
+        }
+    }
+}
