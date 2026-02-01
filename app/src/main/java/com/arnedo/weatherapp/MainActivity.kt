9bbdf403a0085356f97ef1f2dd6284c02afea401
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-01 19:36:50 +0100

    refactor: implement bottom navigation bar

--- app/src/main/java/com/arnedo/weatherapp/MainActivity.kt
+++ app/src/main/java/com/arnedo/weatherapp/MainActivity.kt
@@ -6,15 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.rememberNavController
+import com.arnedo.weatherapp.navigation.AppNavHost
 import com.arnedo.weatherapp.navigation.Destination
 import com.arnedo.weatherapp.ui.theme.WeatherAppTheme
 
@@ -26,31 +34,44 @@ class MainActivity : ComponentActivity() {
             WeatherAppTheme {
                 val navController = rememberNavController()
                 val startDestination = Destination.WEATHER
-                val selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+                var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navOptionsBuilder: NavOptionsBuilder.() -> Unit = {
+                    popUpTo(navController.graph.id) {
+                        inclusive = false
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                            Destination.entries.forEachIndexed { index, destination ->
+                                NavigationBarItem(
+                                    selected = selectedDestination == index,
+                                    onClick = {
+                                        navController.navigate(destination.route, navOptionsBuilder)
+                                        selectedDestination = index
+                                    },
+                                    icon = {
+                                        Icon(
+                                            destination.icon,
+                                            contentDescription = null)
+                                    },
+                                    label = {
+                                        Text(stringResource(destination.labelRes))
+                                    }
+                                )
+                            }
+                        }
+                    }
+                    ) { innerPadding ->
+                    AppNavHost(navController, startDestination, Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherAppTheme {
-        Greeting("Android")
     }
 }
