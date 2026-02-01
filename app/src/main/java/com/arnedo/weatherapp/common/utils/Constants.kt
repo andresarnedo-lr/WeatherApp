be90ae7c3b39e73f19fee5267060824d72d9098e
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-01 20:21:25 +0100

    add Constants.kt for navigation and Retrofit configurations

---
+++ app/src/main/java/com/arnedo/weatherapp/common/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.arnedo.weatherapp.common.utils
+
+//https://api.weatherapi.com/v1/current.json?key=913410e0b77948ec8c7183850260102&q=London&aqi=no
+object Constants {
+    //Navigation
+    const val NAV_WEATHER = "nav_weather"
+    const val NAV_CITIES = "nav_cities"
+
+    //Retrofit
+    const val PARAM_KEY = "913410e0b77948ec8c7183850260102"
+
+}
