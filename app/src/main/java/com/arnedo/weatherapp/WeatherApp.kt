1cf70f5e7e902894bec3965c7abb8a718c7c4b5f
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-03 18:59:19 +0100

    feat: set up Koin dependency injection in Application class

---
+++ app/src/main/java/com/arnedo/weatherapp/WeatherApp.kt
@@ -0,0 +1,15 @@
+package com.arnedo.weatherapp
+
+import android.app.Application
+import com.arnedo.weatherapp.common.di.utilsModule
+import com.arnedo.weatherapp.weather.di.weatherModule
+import org.koin.core.context.GlobalContext.startKoin
+
+class WeatherApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(weatherModule, utilsModule)
+        }
+    }
+}
