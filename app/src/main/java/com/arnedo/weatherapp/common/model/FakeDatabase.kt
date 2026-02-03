df03c25ae77351e341ee0357be0f608ffdb5bf8b
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-03 18:05:31 +0100

    add FakeDatabase for weather city previews

---
+++ app/src/main/java/com/arnedo/weatherapp/common/model/FakeDatabase.kt
@@ -0,0 +1,45 @@
+package com.arnedo.weatherapp.common.model
+
+import com.arnedo.weatherapp.common.entities.WeatherCity
+
+/**
+ * Project: DI Basics
+ * From: com.cursosant.dibasics
+ * Created by: Alain Nicolás Tello.
+ * On: 01/08/25 at 08:51
+ * All rights reserved 2025.
+ *
+ * All my Udemy Courses:
+ * https://www.udemy.com/user/alain-nicolas-tello/
+ * And Frogames formación:
+ * https://cursos.frogamesformacion.com/pages/instructor-alain-nicolas
+ *
+ * Coupons on my Website:
+ * www.alainnicolastello.com
+ */
+
+//val cityPreview = City("Lima", "Perú", 1.2118, -2.1921)
+//
+//fun getAllCityPreview() = listOf(
+//    City("Cdmx", "México", 19.4334565, -99.1331708),
+//    City("Madrid", "España", 40.416775, -3.703790),
+//    cityPreview
+//)
+
+
+val weatherCityPreview = WeatherCity(
+    31f, "Vientos fuertes",
+    22.5f, "","Lima", "Perú"
+)
+
+fun getAllWeatherCityPreview() = listOf(
+    WeatherCity(
+        21f, "Nublado",
+        12.5f, "","CDMX", "México"
+    ),
+    WeatherCity(
+        21f, "Soleado",
+        6f, "","Madrid", "España"
+    ),
+    weatherCityPreview
+)
