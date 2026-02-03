80b6772d287bb1b275d88ad5a9bacdafa1498012
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-03 19:17:29 +0100

    add remote data source module to koin dependency graph

--- app/src/main/java/com/arnedo/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/arnedo/weatherapp/WeatherApp.kt
@@ -2,6 +2,7 @@ package com.arnedo.weatherapp
 
 import android.app.Application
 import com.arnedo.weatherapp.common.di.utilsModule
+import com.arnedo.weatherapp.weather.di.remoteDataSourceModule
 import com.arnedo.weatherapp.weather.di.weatherModule
 import org.koin.core.context.GlobalContext.startKoin
 
@@ -9,7 +10,7 @@ class WeatherApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(weatherModule, utilsModule)
+            modules(weatherModule, utilsModule, remoteDataSourceModule)
         }
     }
 }
