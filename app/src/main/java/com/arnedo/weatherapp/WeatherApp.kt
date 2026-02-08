6c29dfe7f8d31e4c19bc0edcd32deaccbc3d7f3b
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-08 17:07:33 -0300

    add android context to koin start in weatherapp application class

--- app/src/main/java/com/arnedo/weatherapp/WeatherApp.kt
+++ app/src/main/java/com/arnedo/weatherapp/WeatherApp.kt
@@ -1,16 +1,19 @@
 package com.arnedo.weatherapp
 
 import android.app.Application
+import com.arnedo.weatherapp.common.di.localDataSourceModule
 import com.arnedo.weatherapp.common.di.utilsModule
 import com.arnedo.weatherapp.weather.di.remoteDataSourceModule
 import com.arnedo.weatherapp.weather.di.weatherModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
 class WeatherApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(weatherModule, utilsModule, remoteDataSourceModule)
+            androidContext(this@WeatherApp)
+            modules(weatherModule, utilsModule, remoteDataSourceModule, localDataSourceModule)
         }
     }
 }
