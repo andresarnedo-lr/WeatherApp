699dd21a8b97b2b91d1a6367018f71f66a3e649b
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-20 17:57:26 -0300

    add items list property to weather ui state data class

--- app/src/main/java/com/arnedo/weatherapp/weather/viewmodel/WeatherUiState.kt
+++ app/src/main/java/com/arnedo/weatherapp/weather/viewmodel/WeatherUiState.kt
@@ -1,10 +1,12 @@
 package com.arnedo.weatherapp.weather.viewmodel
 
 import com.arnedo.weatherapp.R
+import com.arnedo.weatherapp.common.entities.City
 import com.arnedo.weatherapp.common.entities.WeatherCity
 
 data class WeatherUiState(
     val data : WeatherCity = WeatherCity(),
+    val items : List<City> = emptyList(),
     val inProgress : Boolean = false,
     val msgRes : Int = R.string.msg_empty
     )
