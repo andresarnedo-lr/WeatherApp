98972c99567a31332b04f14924bbc48963f9b2a5
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-20 17:57:40 -0300

    add getWeatherByCity method to fetch weather data from local database

--- app/src/main/java/com/arnedo/weatherapp/weather/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/arnedo/weatherapp/weather/viewmodel/WeatherViewModel.kt
@@ -1,10 +1,15 @@
 package com.arnedo.weatherapp.weather.viewmodel
 
+import android.util.Log.e
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.arnedo.weatherapp.R
+import com.arnedo.weatherapp.common.entities.City
+import com.arnedo.weatherapp.common.entities.WeatherCity
 import com.arnedo.weatherapp.common.utils.FormatUtils
+import com.arnedo.weatherapp.weather.model.LocalDatabase
 import com.arnedo.weatherapp.weather.model.RemoteDatabase
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,35 +17,83 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class WeatherViewModel(
-    private val rdb : RemoteDatabase
+    private val rdb : RemoteDatabase,
+    private val ldb : LocalDatabase
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(WeatherUiState())
     val uiState: StateFlow<WeatherUiState> = _uiState.asStateFlow()
 
+    init {
+        getAllCities()
+    }
 
-    fun searchWeather(name: String) {
-        viewModelScope.launch {
-            _uiState.update { it.copy(inProgress = true) }
-            try {
-                rdb.searchWeatherByName(name) { result ->
-                    if (result != null) {
-                        _uiState.update { it.copy(data = result) }
-                    } else {
-                        _uiState.update { it.copy(msgRes = R.string.weather_search_error) }
-                    }
-
+    private fun getAllCities() {
+        executeAction {
+            ldb.getAllCities { result ->
+                if (result.isNotEmpty()) {
+                    _uiState.update { it.copy(items = result) }
+                } else {
+                    _uiState.update { it.copy(msgRes = R.string.weather_empty_list) }
                 }
-            }catch(e: Exception){
-                _uiState.update {it.copy(msgRes = R.string.weather_general_error)}
-            }finally {
-                _uiState.update { it.copy(inProgress = false) }
             }
         }
     }
-    fun clearMsg(){
+
+
+    fun searchWeather(name: String) {
+        executeAction {
+            rdb.searchWeatherByName(name) { result ->
+                if (result != null) {
+                    _uiState.update { it.copy(data = result) }
+                } else {
+                    _uiState.update { it.copy(msgRes = R.string.weather_search_error) }
+                }
+            }
+        }
+
+    }
+
+    fun saveWeatherCity(weatherCity: WeatherCity) {
+        executeAction {
+            ldb.addWeatherAndCity(weatherCity) { success ->
+                if (success) {
+                    _uiState.update { it.copy(msgRes = R.string.weather_local_save_success) }
+                } else {
+                    _uiState.update { it.copy(msgRes = R.string.weather_local_save_error) }
+                }
+            }
+        }
+    }
+
+    fun getWeatherByCity(city: City) {
+        executeAction {
+            ldb.getWeatherCityByCityId(city.id) { result ->
+                if (result != null) {
+                    _uiState.update { it.copy(data = result) }
+                } else {
+                    _uiState.update { it.copy(msgRes = R.string.weather_local_by_city_error) }
+                }
+            }
+        }
+    }
+
+    fun clearMsg() {
         viewModelScope.launch {
             _uiState.update { it.copy(msgRes = R.string.msg_empty) }
+        }
+    }
 
+
+    private fun executeAction(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            _uiState.update { it.copy(inProgress = true) }
+            try {
+                block()
+            } catch (e: Exception) {
+                _uiState.update { it.copy(msgRes = R.string.weather_general_error) }
+            } finally {
+                _uiState.update { it.copy(inProgress = false) }
+            }
         }
     }
 }
