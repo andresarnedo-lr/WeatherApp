4b0126a3e443d62538a5ad3fce9939cdb763aacd
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-20 18:26:18 -0300

    add networkutils singleton to utils module for network operations

--- app/src/main/java/com/arnedo/weatherapp/common/di/UtilsModule.kt
+++ app/src/main/java/com/arnedo/weatherapp/common/di/UtilsModule.kt
@@ -1,8 +1,10 @@
 package com.arnedo.weatherapp.common.di
 
 import com.arnedo.weatherapp.common.utils.FormatUtils
+import com.arnedo.weatherapp.common.utils.NetworkUtils
 import org.koin.dsl.module
 
 val utilsModule = module {
     single{ FormatUtils() }
+    single{ NetworkUtils(get()) }
 }
