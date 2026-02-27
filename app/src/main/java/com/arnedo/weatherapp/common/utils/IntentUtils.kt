07a5f138d93b3323fee6701cb1b53d894bb645f1
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-27 18:35:34 -0300

    add IntentUtils singleton to utils package

---
+++ app/src/main/java/com/arnedo/weatherapp/common/utils/IntentUtils.kt
@@ -0,0 +1,7 @@
+package com.arnedo.weatherapp.common.utils
+
+class IntentUtils {
+    fun showMap(lat: Double, lon: Double, label: String) {
+        println("Mapa mostrado en : $label($lat,$lon)")
+    }
+}
