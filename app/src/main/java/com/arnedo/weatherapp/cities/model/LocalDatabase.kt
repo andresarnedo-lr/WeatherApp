116064c655fa47f7e684a51c19dcbaaa7dffe266
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-27 18:34:57 -0300

    add suspend function to retrieve all cities from local database

--- app/src/main/java/com/arnedo/weatherapp/cities/model/LocalDatabase.kt
+++ app/src/main/java/com/arnedo/weatherapp/cities/model/LocalDatabase.kt
@@ -1,4 +1,12 @@
 package com.arnedo.weatherapp.cities.model
 
-class LocalDatabase {
+import com.arnedo.weatherapp.common.entities.City
+import com.arnedo.weatherapp.common.model.CityDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class LocalDatabase(private val cityDao : CityDao) {
+    suspend fun getAllCities(onResult:(List<City>) -> Unit) = withContext(Dispatchers.IO){
+        onResult(cityDao.getAllCities())
+    }
 }
