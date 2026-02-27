ef9917cfc45ab8956672bee25cb27b53d6aa94fb
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-27 19:06:26 -0300

    clean up formatting in MyTextTitle component

--- app/src/main/java/com/arnedo/weatherapp/ui/components/Text.kt
+++ app/src/main/java/com/arnedo/weatherapp/ui/components/Text.kt
@@ -2,13 +2,14 @@ package com.arnedo.weatherapp.ui.components
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.arnedo.weatherapp.ui.theme.Typography
 
 @Composable
 
-fun MyTextTitle( titleRes: Int) {
+fun MyTextTitle(titleRes: Int) {
         Text(
             stringResource(titleRes),
             textAlign = TextAlign.Center,
