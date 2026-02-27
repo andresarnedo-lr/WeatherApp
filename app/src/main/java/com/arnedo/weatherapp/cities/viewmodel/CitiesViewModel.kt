14ee9670f66c78fcb19e423cd99c6a8f6d728142
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-02-27 18:36:09 -0300

    add CitiesViewModel class to manage city data in cities package

---
+++ app/src/main/java/com/arnedo/weatherapp/cities/viewmodel/CitiesViewModel.kt
@@ -0,0 +1,44 @@
+package com.arnedo.weatherapp.cities.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arnedo.weatherapp.R
+import com.arnedo.weatherapp.cities.model.LocalDatabase
+import com.arnedo.weatherapp.common.entities.City
+import com.arnedo.weatherapp.common.utils.IntentUtils
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CitiesViewModel(
+    private val db: LocalDatabase,
+    private val utils: IntentUtils
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(CityUiState())
+    fun getUiState(): StateFlow<CityUiState> = _uiState.asStateFlow()
+
+    init {
+        getAllCities()
+    }
+
+    private fun getAllCities() {
+        viewModelScope.launch {
+            _uiState.update { it.copy(inProgress = true) }
+            db.getAllCities { result ->
+                if (result.isNotEmpty()) {
+                    _uiState.update { it.copy(items = result) }
+                } else {
+                    _uiState.update { it.copy(msgRes = R.string.cities_empty_list) }
+                }
+                _uiState.update { it.copy(inProgress = false) }
+            }
+        }
+    }
+
+    fun showMap(city: City) {
+        utils.showMap(city.lat, city.lon, city.name)
+    }
+}
