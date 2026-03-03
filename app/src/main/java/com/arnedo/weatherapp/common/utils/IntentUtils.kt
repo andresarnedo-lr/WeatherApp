f32098a04297f8e0429c30cb0af1713c022e7ac5
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-03-03 18:05:33 -0300

    add startIntent method to IntentUtils for launching intents with fallback toast

--- app/src/main/java/com/arnedo/weatherapp/common/utils/IntentUtils.kt
+++ app/src/main/java/com/arnedo/weatherapp/common/utils/IntentUtils.kt
@@ -1,7 +1,29 @@
 package com.arnedo.weatherapp.common.utils
 
-class IntentUtils {
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.net.toUri
+import com.arnedo.weatherapp.R
+
+class IntentUtils(private val context: Context) {
     fun showMap(lat: Double, lon: Double, label: String) {
-        println("Mapa mostrado en : $label($lat,$lon)")
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.apply {
+            data = "geo: 0,0?q=$lat,$lon($label)".toUri()
+            `package` = "com.google.android.apps.maps"
+        }
+        startIntent(intent)
+    }
+
+    fun startIntent(intent: Intent){
+        val chooser: Intent = Intent.createChooser(intent, context.getString(R.string.app_name))
+        chooser.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        if(intent.resolveActivity(context.packageManager) != null){
+            context.startActivity(chooser)
+        }else {
+            Toast.makeText(context, context.getString(R.string.msg_no_app), Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
