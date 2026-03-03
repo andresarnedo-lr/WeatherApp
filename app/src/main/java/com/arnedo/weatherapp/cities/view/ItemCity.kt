5507f32b38c4eb55537862c104b3269723e6b4bb
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-03-03 18:37:48 -0300

    add swipe-to-dismiss functionality with delete action in itemcityview

--- app/src/main/java/com/arnedo/weatherapp/cities/view/ItemCity.kt
+++ app/src/main/java/com/arnedo/weatherapp/cities/view/ItemCity.kt
@@ -1,18 +1,28 @@
 package com.arnedo.weatherapp.cities.view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowOutward
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.arnedo.weatherapp.common.entities.City
 import com.arnedo.weatherapp.common.model.cityPreview
+import com.arnedo.weatherapp.ui.theme.CommonPaddingListItem3Vertical
 import com.arnedo.weatherapp.ui.theme.CommonPaddingMin
 import com.arnedo.weatherapp.ui.theme.Typography
 import com.arnedo.weatherapp.ui.theme.WeatherAppTheme
@@ -20,18 +30,49 @@ import com.arnedo.weatherapp.ui.theme.WeatherAppTheme
 @Composable
 fun ItemCityView(
     city: City,
-    onMap: (City) -> Unit
+    onMap: (City) -> Unit,
+    onRemove: (City) -> Unit
 ){
-    Card(modifier = Modifier.padding(CommonPaddingMin)) {
-        Row{
-            Text(text = city.toString(),
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(CommonPaddingMin),
-                style = Typography.headlineSmall
-            )
-            IconButton(onClick = {onMap(city)}) {
-                Icon(Icons.Default.ArrowOutward, contentDescription = null)
+    val swipeToDismissBoxState = rememberSwipeToDismissBoxState(
+        confirmValueChange = {
+            if(it == SwipeToDismissBoxValue.EndToStart) {
+                onRemove(city)
+            }
+            it != SwipeToDismissBoxValue.EndToStart
+        }
+    )
+
+    SwipeToDismissBox(
+        state = swipeToDismissBoxState,
+        modifier = Modifier.fillMaxSize(),
+        enableDismissFromStartToEnd = false,
+        backgroundContent = {
+            when(swipeToDismissBoxState.dismissDirection){
+                SwipeToDismissBoxValue.EndToStart -> {
+                    Icon(imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Red)
+                            .wrapContentSize(Alignment.CenterEnd)
+                            .padding(CommonPaddingListItem3Vertical),
+                        tint = Color.White)
+                }
+                else -> {}
+            }
+        }
+        ) {
+        Card(modifier = Modifier.padding(CommonPaddingMin)) {
+            Row{
+                Text(text = city.toString(),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(CommonPaddingMin),
+                    style = Typography.headlineSmall
+                )
+                IconButton(onClick = {onMap(city)}) {
+                    Icon(Icons.Default.ArrowOutward, contentDescription = null)
+                }
             }
         }
     }
@@ -41,6 +82,6 @@ fun ItemCityView(
 @Composable
 fun ItemCityPreview(){
     WeatherAppTheme() {
-        ItemCityView(cityPreview){}
+        ItemCityView(cityPreview, {}){}
     }
 }
