601cf7ab84e9fa3a49200a78bf41d44ee4261104
Andres Arnedo <alfredoandresarnedo@gmail.com>
2026-03-03 18:06:23 -0300

    add getAllCitiesRealtime method and deleteCityAndWeather function in localdatabase

--- app/src/main/java/com/arnedo/weatherapp/cities/model/LocalDatabase.kt
+++ app/src/main/java/com/arnedo/weatherapp/cities/model/LocalDatabase.kt
@@ -2,11 +2,21 @@ package com.arnedo.weatherapp.cities.model
 
 import com.arnedo.weatherapp.common.entities.City
 import com.arnedo.weatherapp.common.model.CityDao
+import com.arnedo.weatherapp.common.model.WeatherCityDao
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
-class LocalDatabase(private val cityDao : CityDao) {
-    suspend fun getAllCities(onResult:(List<City>) -> Unit) = withContext(Dispatchers.IO){
-        onResult(cityDao.getAllCities())
+class LocalDatabase(
+    private val cityDao: CityDao,
+    private val weatherCityDao: WeatherCityDao
+) {
+    fun getAllCitiesRealtime(): Flow<List<City>> = cityDao.getAllCitiesRealtime()
+
+    suspend fun deleteCityAndWeather(
+        city: City,
+        onResult: (Boolean) -> Unit
+    ) = withContext(Dispatchers.IO) {
+        onResult(weatherCityDao.deleteCityAndWeather(city) > 0)
     }
 }
